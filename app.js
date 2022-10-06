bf93261fb197d7ab81b798619dc5cc720511bc1b
Andras Bacsai <andras.bacsai@protonmail.com>
2022-10-06 09:56:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.send(`Hello World!<br><p>I have been loaded ${num} times.</p>`);
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Example app listening on port 3000!');
 });
